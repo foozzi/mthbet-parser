682b7416fd232a881e9beb80875cdfc5e4ce5188
foozzi <foozzione@gmail.com>
2017-02-14 00:23:40 +0000

    fix crash in phantomjs

--- main.py
+++ main.py
@@ -17,31 +17,70 @@
 from selenium import webdriver
 import selenium.webdriver.support.ui as ui
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException
+
 import time
 
-def start():
-    driver = webdriver.Firefox()
-    driver.set_window_size(1120, 550)
-    driver.get("https://www.mthbet28.com/su/virtual-sports/football")
-    #time.sleep(8)
-    #switch to iframe 1
-    frame1 = driver.find_element_by_xpath("//iframe[1]"); 
-    driver.switch_to.frame(frame1);
-    #switch to iframe 2
-    frame2 = driver.find_element_by_xpath("//iframe[1]"); 
-    driver.switch_to.frame(frame2);
-    print getCurrentTour(driver)
-    #html = driver.find_element_by_id('matchdays_container').get_attribute('innerHTML')
-    #driver.find_element_by_id("search_button_homepage").click()
-    #print html
-    driver.quit()
 
-def getCurrentTour(driver):
-    tour = driver.find_element_by_id('matchdays_container').find_element_by_class_name('current').get_attribute('innerText')
-    if tour:
-        return tour
-    else:
-        print 'Can\'t find current tour'
+
+class parser():
+    parse_tour = 0
+    frame1 = ''
+    frame2 = ''
+    
+    def start(self):
+        driver = webdriver.Firefox() #or switch to PhantomJS
+        driver.set_window_size(1120, 550)
+        driver.get("https://www.mthbet28.com/su/virtual-sports/football")
+        
+        #time.sleep(10)
+        i = 1
+        time.sleep(3)
+        
+        while i < 29:
+            self.parse_tour = i
+            print 'Check ' + str(i) + ' tour'
+            driver.switch_to_default_content()
+            #switch to iframe 1
+            self.frame1 = self.getFrame(driver, "//iframe[1]")
+            driver.switch_to.frame(self.frame1)
+            #switch to iframe 2
+            self.frame2 = self.getFrame(driver, "//iframe[1]")
+            driver.switch_to.frame(self.frame2)
+            driver.find_element_by_id('matchday' + str(i)).click()
+            self.getKofTour(driver)
+            i += 1
+        
+        #print getCurrentTour(driver)
+        #html = driver.find_element_by_id('matchdays_container').get_attribute('innerHTML')
+        #driver.find_element_by_id("search_button_homepage").click()
+        #print html
+        driver.quit()
+        
+    def getFrame(self, driver, frame):
+        frame1 = driver.find_element_by_xpath(frame)
+        return frame1
+
+    def getCurrentTour(self, driver):
+        tour = driver.find_element_by_id('matchdays_container').find_element_by_class_name('current').get_attribute('innerText')
+        if tour:
+            return tour
+        else:
+            print 'Can\'t find current tour'
+            
+    def getKofTour(self, driver):
+        match = []
+        driver.switch_to_default_content()
+        self.frame1 = self.getFrame(driver, "//iframe[1]")
+        driver.switch_to.frame(self.frame1)
+        time.sleep(2)
+        games_container = driver.find_element_by_id("rgs-matchlist-container")
+        games = games_container.find_element_by_class_name("rgs-matchlist-item")
+        games_name = games.find_element_by_class_name("matchlist-item-teams").get_attribute('innerText')
+        print self.parse_tour
+        print games_name
+        #print driver.find_element_by_id('mainTab').get_attribute('innerHTML')
 
 if __name__ == "__main__":
-    start()
+    Bet = parser()
+    Bet.start()
